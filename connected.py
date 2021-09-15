0e6645d96026fd1f70e99641f214d39f90953463
cuamll <callum.gray.10@ucl.ac.uk>
2021-09-15 14:52:25 +0100

    add circles.py and shapefill.py, remove unneeded stuff in connected.py

--- connected.py
+++ connected.py
@@ -2,41 +2,20 @@ import cv2
 import sys
 import numpy as np
 
+np.set_printoptions(threshold=sys.maxsize)
 img = cv2.imread('004_new.bmp', 0)
 img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)[1]  # ensure binary
+# img is now a big array that's either 0 or 255
 output = cv2.connectedComponentsWithStats(img)
 (num_labels, labels_im, stats, centroids) = output
-print("Number of labels = {}".format(num_labels))
-np.set_printoptions(threshold=sys.maxsize)
-print("labels[0] = {}".format(labels_im[0]))
-print(np.max(labels_im))
-
-# def imshow_components(labels):
-#     # Map component labels to hue val
-#     for i in range(num_labels):
-#         label_hue = np.uint8(179*labels[i]/np.max(labels))
-#         blank_ch = 255*np.ones_like(label_hue)
-#         labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
-
-#         # cvt to BGR for display
-#         labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
-
-#         # set bg label to black
-#         labeled_img[label_hue==0] = 0
-
-#         cv2.imwrite('labeled_{:3d}.png'.format(i), labeled_img)
-#         # cv2.waitKey()
+print("Number of aggregates = {}".format(num_labels))
 
 for i in range(0, num_labels):
-        # if this is the first component then we examine the
-        # *background* (typically we would just ignore this
-        # component in our loop)
-        if i == 0:
-        	text = "examining component {}/{} (background)".format(
-        		i + 1, num_labels)
+        if i == 0: # background
+            continue
         # otherwise, we are examining an actual connected component
         else:
-        	text = "examining component {}/{}".format( i + 1, num_labels)
+        	text = "examining component {}/{}".format(i + 1, num_labels)
         # print a status message update for the current connected
         # component
         # extract the connected component statistics and centroid for
@@ -62,20 +41,3 @@ for i in range(0, num_labels):
         cv2.imwrite("components/{:3d}.png".format(i), output)
         cv2.imwrite("components/connected_component_{:3d}.png".format(i), componentMask)
 
-def imshow_components(labels):
-    # Map component labels to hue val
-    label_hue = np.uint8(179*labels/np.max(labels))
-    print(label_hue[0])
-    blank_ch = 255*np.ones_like(label_hue)
-    labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
-
-    # cvt to BGR for display
-    labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
-
-    # set bg label to black
-    labeled_img[label_hue==0] = 0
-
-    cv2.imwrite('labeled.png', labeled_img)
-    # cv2.waitKey()
-
-imshow_components(labels_im)

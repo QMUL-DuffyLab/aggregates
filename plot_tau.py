cec335649d105f5f0df1eee963cb7d5b371fd28a
callum <callum.gray.10@ucl.ac.uk>
2022-03-30 11:08:44 +0100

    add checks to plot_tau so it won't break all the time

--- plot_tau.py
+++ plot_tau.py
@@ -13,13 +13,28 @@ def forward(x):
     '''convert fluence to average excitons per trimer'''
     return x * 1.1E-14
 
-lifetimes = np.loadtxt("{}/lifetimes.dat".format(file_path))
-
 fig, ax1 = plt.subplots()
-# plt.plot(fluences, lifetimes/1000., label=r'$ \left< \tau_{\text{avg.}} \right> $', marker='o', ms=6.0, lw=3.0)
-plt.errorbar(fluences, lifetimes[:, 0]/1000., yerr=lifetimes[:, 1]/1000.,
-        label=r'$ \left< \tau_{\text{avg.}} \right> $', 
-        elinewidth=0.5, capsize=2.0, marker='o', ms=6.0, lw=3.0)
+if os.path.isfile("{}/lifetimes.dat".format(file_path)):
+    lifetimes = np.loadtxt("{}/lifetimes.dat".format(file_path))
+    plt.errorbar(fluences, lifetimes[:, 0]/1000., yerr=lifetimes[:, 1]/1000.,
+            label=r'$ \left< \tau_{\text{avg.}} \right> $', 
+            elinewidth=0.5, capsize=2.0, marker='o', ms=6.0, lw=3.0)
+if os.path.isfile("{}/mono_tau.dat".format(file_path)):
+    lifetimes = np.loadtxt("{}/mono_tau.dat".format(file_path))
+    plt.errorbar(fluences, lifetimes[:, 0]/1000., yerr=lifetimes[:, 1]/1000.,
+            label=r'$ \left< \tau_{\text{mono.}} \right> $', 
+            elinewidth=0.5, capsize=2.0, marker='o', ms=6.0, lw=3.0)
+if os.path.isfile("{}/bi_tau.dat".format(file_path)):
+    lifetimes = np.loadtxt("{}/bi_tau.dat".format(file_path))
+    plt.errorbar(fluences, lifetimes[:, 0]/1000., yerr=lifetimes[:, 1]/1000.,
+            label=r'$ \left< \tau_{\text{bi.}} \right> $', 
+            elinewidth=0.5, capsize=2.0, marker='o', ms=6.0, lw=3.0)
+if os.path.isfile("{}/tri_tau.dat".format(file_path)):
+    lifetimes = np.loadtxt("{}/tri_tau.dat".format(file_path))
+    plt.errorbar(fluences, lifetimes[:, 0]/1000., yerr=lifetimes[:, 1]/1000.,
+            label=r'$ \left< \tau_{\text{tri.}} \right> $', 
+            elinewidth=0.5, capsize=2.0, marker='o', ms=6.0, lw=3.0)
+
 plt.errorbar(exp[:, 0], exp[:, 1], yerr=exp[:, 2], label=r'Exp.', 
         elinewidth=0.5, capsize=2.0, marker='o', ms=6.0, lw=3.0)
 plt.grid()
@@ -45,4 +60,4 @@ ax2.set_yticks([4., 3., 2.5, 2., 1.5, 1., 0.6])
 ax2.set_yticklabels(["4", "3", "2.5", "2", "1.5", "1", "0.6"])
 plt.minorticks_off()
 ax1.minorticks_off()
-fig.savefig("{}/tau_loglog.pdf".format(os.path.dirname(file_path)))
+fig.savefig("{}/tau_loglog.pdf".format(file_path))

99403cb56a16029db43433eb7d95dd4a89d2d380
callum <callum.gray.10@ucl.ac.uk>
2022-06-09 14:48:05 +0100

    improve argparse - required arguments with choices, add fluences as optional arg, add fit_only and files_only flags

--- main.py
+++ main.py
@@ -15,9 +15,22 @@ from kmc import Pulse, Rates, Iteration
 if __name__ == "__main__":
     start_time = time.monotonic()
     parser = argparse.ArgumentParser(description="set up aggregate simualtion")
+    # required arguments
+    parser.add_argument('-q', '--rho_q', type=float, required=True,
+            help=r'Density of quenchers \rho_q')
+    parser.add_argument('-l', '--lattice', type=str, required=True,
+            choices=['line', 'square', 'honeycomb', 'hex'],
+            help="Lattice type. Options are: 'hex', 'honeycomb', 'square', 'line'")
+    parser.add_argument('-m', '--model', type=str, required=True,
+            choices=[*defaults.rates_dict],
+            help='''
+Quenching model to use. Options are:
+'hop_only', 'irrev', 'rev', 'fast_irrev', 'slow', 'exciton'.
+See defaults.py for specific numbers''')
+    # optional arguments
     parser.add_argument('-pr', '--protein_radius', type=float,
             default=defaults.protein_r,
-            help="Radius of the protein - only relevant when packing real aggregates")
+            help="Radius of the protein (nm) - only relevant when packing real aggregates")
     parser.add_argument('-n', '--n_trimers', type=int, default=defaults.n_trimers,
             help="Number of trimers (approximate) to put in the aggregate")
     parser.add_argument('-mc', '--max_count', type=int, default=defaults.max_count,
@@ -28,32 +41,30 @@ if __name__ == "__main__":
             help="Full width half maximum of the pulse (ps)")
     parser.add_argument('-pm', '--pulse_mean', type=float, default=defaults.pulse_mu,
             help="Peak time of the pulse (ps)")
-    parser.add_argument('-q', '--rho_q', type=float, required=True,
-            help=r'Density of quenchers \rho_q')
-    parser.add_argument('-l', '--lattice', type=str, required=True,
-            help="Lattice type - options are: 'hex', 'honeycomb', 'square', 'line'")
-    parser.add_argument('-m', '--model', type=str, required=True,
+    parser.add_argument('-f', '--fluences', type=float, nargs='+',
+            default=defaults.fluences,
             help='''
-Quenching model to use - options are:
-'hop_only', 'irrev', 'rev', 'fast_irrev', 'slow', 'exciton'.
-See defaults.py for specific numbers''')
-    parser.add_argument('--fit_only', type=argparse.BooleanOptionalAction,
+Fluence(s) to use for the pulse (photons per pulse per cm^2).
+Note that we consider a 485nm laser here; if this changes, the cross-section
+per trimer will also need to be changed.
+''')
+    parser.add_argument('--fit_only', action=argparse.BooleanOptionalAction,
             help="Pass to disable running the code and just re-fit the data")
+    parser.add_argument('--files_only', action=argparse.BooleanOptionalAction,
+            help="Pass to disable the running and the fits and just generate input files")
+
     args = parser.parse_args()
     print(args)
-    run = True # set to true to run, false to generate input files
-    # fluences given here as photons per pulse per unit area - 485nm
-    fluences = [6.07E12, 3.03E13, 6.24E13, 1.31E14,
-            1.9E14, 3.22E14, 6.12E14, 9.48E14]
     # annihilation, pool decay, pq decay, q decay
     path = "out/{}/{}".format(args.model, args.lattice)
     os.makedirs(path, exist_ok=True)
     rates = defaults.rates_dict[args.model]
     pulse = Pulse(fwhm=args.pulsewidth, mu=args.pulse_mean)
-    mt = open("{}/{:3.2f}_mono_tau.dat".format(path, args.rho_q), "w")
-    bt = open("{}/{:3.2f}_bi_tau.dat".format(path, args.rho_q), "w")
-    tt = open("{}/{:3.2f}_tri_tau.dat".format(path, args.rho_q), "w")
-    for fluence in defaults.fluences:
+    if not args.files_only:
+        mt = open("{}/{:3.2f}_mono_tau.dat".format(path, args.rho_q), "w")
+        bt = open("{}/{:3.2f}_bi_tau.dat".format(path, args.rho_q), "w")
+        tt = open("{}/{:3.2f}_tri_tau.dat".format(path, args.rho_q), "w")
+    for fluence in args.fluences:
         print("Fluence = {:4.2E}".format(
             fluence))
         os.makedirs(path, exist_ok=True)
@@ -64,12 +75,12 @@ See defaults.py for specific numbers''')
             verbose = False
             # note - second parameter here is the nn cutoff. set to 0 to
             # disable excitation hopping between trimers
-            agg = theoretical_aggregate(args.protein_radius, 
+            agg = theoretical_aggregate(args.protein_radius,
                     2.01 * args.protein_radius, args.lattice, args.n_trimers)
             it = Iteration(agg, rates, pulse, args.rho_q,
                     path, fluence, args.binwidth, args.max_count,
                     verbose=verbose)
-            if (run):
+            if not args.files_only:
                 subprocess.run(['./f_iter', it.params_file], check=True)
 
         if os.path.isfile("{}/{}_counts.dat".format(path, file_prefix)):
@@ -93,9 +104,11 @@ See defaults.py for specific numbers''')
             np.savetxt(bt, np.array(bi_tau).reshape(1, 3))
             np.savetxt(tt, np.array(tri_tau).reshape(1, 3))
 
-    mt.close()
-    bt.close()
-    tt.close()
+    if not args.files_only:
+        mt.close()
+        bt.close()
+        tt.close()
+        subprocess.run(['python', 'plot_tau.py', '{}'.format(path), '{:3.2f}'.format(args.rho_q)], check=True)
+
     end_time = time.monotonic()
     print("Total time elapsed: {}".format((end_time - start_time)))
-    subprocess.run(['python', 'plot_tau.py', '{}'.format(path), '{:3.2f}'.format(args.rho_q)], check=True)
